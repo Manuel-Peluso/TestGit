56efd6fe1291cc9248109939be6afbc6c8aa62c2
Manuel <manuelpeluso02@gmail.com>
2023-06-07 10:24:21 +0200

    Secondo commit

--- src/it/betacom/main/Main.java
+++ src/it/betacom/main/Main.java
@@ -3,7 +3,7 @@ package it.betacom.main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Avvio del programma!");
+		System.out.println("Avvio del programma main!");
 
 	}
 
